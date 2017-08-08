05c301ce96515f6f7f1b313dc788f5c7c6e95f44
Lance Whatley <whatl3y@gmail.com>
2017-08-08 17:35:21 -0400

    Add global script comment

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 'use strict';
 
 var _minimist = require('minimist');
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 const plumber = require('gulp-plumber')
+const insert = require('gulp-insert')
 // const replace = require('gulp-replace')
 // const sourcemaps = require('gulp-sourcemaps')
 // const uglify = require('gulp-uglify')
 
-gulp.task('build', function() {
+gulp.task('transpile', function() {
   return gulp.src("./src/**/*.js")
     // .pipe(sourcemaps.init())
     .pipe(plumber())
@@ -14,3 +15,11 @@ gulp.task('build', function() {
     // .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest("./dist"))
 })
+
+gulp.task('index', function() {
+  return gulp.src("./dist/index.js")
+    .pipe(insert.prepend("#!/usr/bin/env node\n\n"))
+    .pipe(gulp.dest("./dist"))
+})
+
+gulp.task('build', ['transpile', 'index'])
